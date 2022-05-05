a77492021f26c9ab816d6668998751ae372908d8
Michał Kozłowski <erpus@wp.pl>
2022-05-05 20:03:44 +0200

    modele 1

--- projekt/base/models.py
+++ projekt/base/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id_uzytkownika = models.BigAutoField(primary_key=True)
+    imie = models.CharField(max_length=30)
+    nazwisko = models.CharField(max_length=35)
+    pseudonim = models.CharField(max_length=20)
+    wiek = models.IntegerField()
+
+class pushups(models.Model):
+    id_uzytkownika = models.ForeignKey(User, on_delete = models.CASCADE)
+    data = models.DateTimeField(auto_now_add=True)
+    powtorzenia = models.IntegerField()
+    seria = models.IntegerField()
+
+class ranking(models.Model):
+    id_uzytkownika = models.ForeignKey(User, on_delete = models.CASCADE)
+    sumaPowt = models.IntegerField()
+    iloscSerii = models.IntegerField()
--- projekt/base/views.py
+++ projekt/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'base/home.html')
 
 def user(request):
-    return render(request, 'user.html')
+    return render(request, 'base/user.html')
