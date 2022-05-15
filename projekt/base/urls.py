31ad189eadd9323207d40abe5d5e55ef732ef876
Michał Kozłowski <erpus@wp.pl>
2022-05-15 19:56:24 +0200

    redirecty i pocztek loginu

--- projekt/base/urls.py
+++ projekt/base/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.home, name = "home"),
-	path('user/', views.user, name = "user"),
+	path("", views.home, name = "home"),
+	path("user/", views.user, name = "user"),
+	path("ranking/", views.ranking, name ="ranking"),
+	path("kontakt/", views.kontakt, name ="kontakt"),
+	path("login/", views.loginPage, name="login"),
+	path("logout/", views.logoutUser, name="logout"),
 ]
--- projekt/base/views.py
+++ projekt/base/views.py
@@ -1,7 +1,42 @@
-from django.shortcuts import render
+﻿from django.shortcuts import render
+from .models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get("usesrname")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "Nie ma takiego użytkownika")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Błędna nazwa użytkownika lub hasło")
+    context= {}
+    return render(request, "base/login.html", context)
+
+def logoutUser(request):
+    logout(request)
+    return rediret("home")
 
 def home(request):
     return render(request, 'base/home.html')
 
-def user(request):
-    return render(request, 'base/user.html')
+def user(request, pk):
+    user = User.objects.get(id = pk)
+    context = {'user': user}
+    return render(request, 'base/user.html', context)
+
+def ranking(request):
+    return render(request, "base/ranking.html")
+
+def kontakt(request):
+    return render(request, "base/kontakt.html")
