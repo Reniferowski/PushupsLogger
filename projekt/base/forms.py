819e46749f8a78e6dce07349f2e61bff0a870b30
Bartosz Sietejko <barto32224@wp.pl>
2022-05-25 22:36:13 +0200

    widgets and labels

--- projekt/base/forms.py
+++ projekt/base/forms.py
@@ -1,11 +1,23 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
+from django import forms
+from django.contrib.auth.forms import UsernameField
 
 class UserRegister(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control',"placeholder":"Pseudonim"}),
+            'email': forms.TextInput(attrs={'class': 'form-control',"placeholder":"Adres Email"}),
+        }
+        labels = {
+            'username': "Pseudonim",
+            'email': "Adres Email",
+        }
+
+
 
 class UserForm(ModelForm):
     class Meta:
