6ab81f540b8107e05ce715b399f7a4c32ecbd435
Bartosz Sietejko <barto32224@wp.pl>
2022-05-29 21:36:51 +0200

    override user template at admin panel

--- projekt/base/admin.py
+++ projekt/base/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import User, Ranking, Pushups
 
-admin.site.register(User)
+
 admin.site.register(Ranking)
 admin.site.register(Pushups)
+@admin.register(User)
+class userAdmin(admin.ModelAdmin):
+    fields = ("username","imie", "nazwisko", "email",)
+    list_display = ("username", "nazwisko", "email",)
+    ordering = ("nazwisko",)
+    search_fields = ("nazwisko", "email",)
