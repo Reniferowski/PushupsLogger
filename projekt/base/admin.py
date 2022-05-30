c198969981b1e9f5de7ef1eac5da7e88053b2a20
Bartosz Sietejko <barto32224@wp.pl>
2022-05-30 18:44:22 +0200

    pushup table changes in admin panel

--- projekt/base/admin.py
+++ projekt/base/admin.py
@@ -4,10 +4,17 @@ from .models import User, Ranking, Pushups
 
 
 admin.site.register(Ranking)
-admin.site.register(Pushups)
 @admin.register(User)
 class userAdmin(admin.ModelAdmin):
     fields = ("username","imie", "nazwisko", "email", "opis", "avatar")
     list_display = ("username", "nazwisko", "email",)
     ordering = ("nazwisko",)
     search_fields = ("nazwisko", "email",)
+
+@admin.register(Pushups)
+class pushupsAdmin(admin.ModelAdmin):
+    fields = ("powtorzenia", "seria")
+    list_display = ("id_uzytkownika", "data")
+    list_filter = ("data",)
+    ordering = ("id_uzytkownika",)
+    search_fields = ("data", "id_uzytkownika")
