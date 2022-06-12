e57f89afebf9cb87ce928b33ea26b6d0168b35e8
Bartosz Sietejko <barto32224@wp.pl>
2022-06-12 22:37:09 +0200

    add allowed_hosts

--- projekt/projekt/settings.py
+++ projekt/projekt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f14si!mwz15w-dhjj=o-i+8fja)%pnqx0av#uxqg_110ff!=63
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost"]
 
 
 # Application definition
